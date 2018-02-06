3a7da4b40573f41f9da8bf4ddd54171eea9d3090
Santiago Ruiz Zapata <san_rz89@hotmail.com>
2018-02-06 10:19:01 +0100

    This ffile includes the radius calculus function

--- misc_functions.py
+++ misc_functions.py
@@ -1,6 +1,8 @@
 from datetime import date
 import pandas as pd
 from bokeh.transform import factor_cmap
+from math import radians, cos, sin, asin, sqrt
+import numpy as np
 
 def convert_list_to_date(list):
     new_list = []
@@ -67,3 +69,51 @@ def return_value_list(locations, start='2017-1-1', end='2017-12-31'):
             consumption = df_temp['delta_total'].sum()
             value_list.append(consumption)
     return value_list
+
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+
+def select_events(lon, lat, data_cc, radius):
+    """
+    This function calcualte the distance of everu event based on the latitud and longitud of the location selected. This equation os based on
+    the haversine distance:
+        https://en.wikipedia.org/wiki/Haversine_formula
+        
+    Parameters
+    ---------------------------------
+        lon: longitid of the eLog location
+        lat: latitud of the eLog location
+        data_cc: data frame with events
+        radius: distance in Km where the events are selected.
+        
+    Return
+    ---------------------------------
+        events_selected: vector with the Id of the events selected
+    """
+    
+    data_cc['haversine_distance'] = np.vectorize(haversine)(lon, lat,data_cc['Longitude'], data_cc['Latitude'])
+    data_cc = data_cc[data_cc['haversine_distance'] <= radius]
+    
+    return data_cc
+
+
+if __name__ == "__main__":
+    data_cc = pd.read_csv('data/Data_heat_maps/Customer Contacts/limited_occ_with_gps_time.csv', sep = ';')
+    events_selected = select_events(5.487716, 51.479160, data_cc, 30)
