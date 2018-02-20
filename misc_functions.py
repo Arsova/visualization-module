5b753aee41f010f7fa143fe0a689a001b9412df0
Nitin <nitinnarayan1501@gmail.com>
2018-02-20 12:19:14 +0100

    changed radius formula

--- misc_functions.py
+++ misc_functions.py
@@ -1,7 +1,7 @@
 from datetime import date
 import pandas as pd
 from bokeh.transform import factor_cmap
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt,atan2
 import numpy as np
 pd.options.mode.chained_assignment = None
 
@@ -100,8 +100,10 @@ def haversine(lon1, lat1, lon2, lat2):
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
+    # a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    # c = 2 * asin(sqrt(a))
+    a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
